3fdd3c09afcae6551496cc15ddcbab742b619d8b
EDUAR DORADO <eduar.dorado@telefonica.com>
2022-03-18 19:32:59 -0500

    smooth scroll, preventDefault added

--- js/app.js
+++ js/app.js
@@ -21,10 +21,12 @@
 const uls=document.querySelectorAll('ul');
 const sections=document.querySelectorAll('section')
 buildNav();
-
 document.addEventListener("scroll", function() {
   makeActive();
 });
+
+
+
 
 /**
 * @description build the navbar from the sections
@@ -47,10 +49,10 @@ function buildNav() {
 * @event  click event  on "li" tag
 */
 function scrollToSection(event) {
-  let li=event.target
-  let nameSection=li.textContent;
+  const li=event.target
+  const nameSection=li.textContent;
   const section = document.querySelector(`[data-nav="${nameSection}"]`);
-  section.scrollIntoView();
+  section.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   // console.log("scrolling",event.target,nameSection,section)
 }
 
@@ -61,20 +63,55 @@ function scrollToSection(event) {
 * @global  sections 
 */
 function makeActive() {
-    for (const section of sections) {
-      const box = section.getBoundingClientRect();
-      let titleLi=section.dataset.nav
-      let currentLi=document.getElementById(`li${titleLi}`)
-      // You can play with the values in the "if" condition to further make it more accurate. 
-      if (box.top <= 150 && box.bottom >= 150) {
-        // Apply active state on the current section and the corresponding Nav link.
-        section.classList.add('your-active-class');
-        currentLi.classList.add('menu__link')
-       
-      } else {
-        section.classList.remove('your-active-class');
-        currentLi.classList.remove('menu__link')
-        // Remove active state from other section and corresponding Nav link.
-      }
+  for (const section of sections) {
+    let box = section.getBoundingClientRect();
+    let titleLi=section.dataset.nav
+    let currentLi=document.getElementById(`li${titleLi}`)
+    if (box.top <= 150 && box.bottom >= 150) {
+      // Apply active state on the current section and the corresponding Nav link.
+      section.classList.add('your-active-class');
+      currentLi.classList.add('menu__link')
+    } else {
+      section.classList.remove('your-active-class');
+      currentLi.classList.remove('menu__link')
+      // Remove active state from other section and corresponding Nav link.
+      scrollStop(stopEvent(currentLi),300)
     }
   }
+
+   
+}
+
+
+
+ /*!
+ * @description Run a callback function after scrolling has stopped
+ * @param  {Function} callback The callback function to run after scrolling
+ * @param  {Integer}  refresh  How long to wait between scroll events [optional]
+ */
+function scrollStop (callback, refresh = 66) {
+
+	// Make sure a valid callback was provided
+	if (!callback || typeof callback !== 'function') return;
+
+	// Setup scrolling variable
+	let isScrolling;
+	// Listen for scroll events
+	window.addEventListener('scroll', function () {
+		// Clear our timeout throughout the scroll
+		window.clearTimeout(isScrolling);
+		// Set a timeout to run after scrolling ends
+		isScrolling = setTimeout(callback, refresh);
+	}, false);
+
+}
+
+/**
+* @description stop scroll event when scrolling has stopped
+* @param {element} element - current li
+*/
+function stopEvent(element) {
+  element.addEventListener("click", function(event){
+           event.preventDefault()
+  });
+}
